ac59e0b03b66dce63dc8a3ded6f1176493d5e427
kakashi <thanhtai.tttgalaxy@gmail.com>
2024-04-08 19:02:31 +0700

    Update Banner.tsx

--- src/containers/home/Banner.tsx
+++ src/containers/home/Banner.tsx
@@ -125,7 +125,7 @@ export const Banner = () => {
               <AnimationView.FadeIn delay={4000}>
                 <div className='flex w-full h-full justify-center items-end'>
                   <div className='z-40'>
-                    <Animates.RippleButton className='mb-4 flex flex-col items-center justify-center cursor-pointer' id="project-section-btn"
+                    <Animates.RippleButton className='mb-8 lg:mb-4 flex flex-col items-center justify-center cursor-pointer' id="project-section-btn"
                       onClick={() => homeDispatcher &&
                         homeDispatcher({
                           type: HomeActionType.updateActiveSection,
