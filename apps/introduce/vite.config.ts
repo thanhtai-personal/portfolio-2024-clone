928be37b453f4f491ac5623b0e7ce6598548ca61
tai.tranth <thanhtai.tttgalaxy@gmail.com>
2024-04-12 16:31:06 +0700

    update build config

--- apps/introduce/vite.config.ts
+++ apps/introduce/vite.config.ts
@@ -1,29 +1,16 @@
 import react from '@vitejs/plugin-react';
-// import { glob } from "glob";
-// import { fileURLToPath } from "node:url";
-// import path, { extname, relative, resolve } from "path";
 import path from "path";
 import { defineConfig } from 'vite';
-import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), libInjectCss(), dts({ include: ["cordova-build/www"] })],
+  plugins: [react(), libInjectCss()],
   build: {
-    outDir: "release/www",
+    outDir: "dist",
     chunkSizeWarningLimit: 10240,
     sourcemap: true,
-    // modulePreload: { polyfill: true }
   },
-  // server: {
-    // middlewareMode: true,
-    // port: 5173,
-    // warmup: {
-    //   clientFiles: [],
-    //   ssrFiles: [],
-    // }
-  // },
   resolve: {
     alias: {
       "@/components": path.resolve(__dirname, "src", "components"),
